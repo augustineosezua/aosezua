de5467ef84e87613a00987b34142907b6ccd4a7d
Augustine Osezua <augustineosezua1@gmail.com>
2025-08-21 16:01:59 -0400

    git init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className='w-screen h-screen flex items-center justify-center'>
+        <h1 className='text-4xl font-bold'>Augustine Osezua</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
