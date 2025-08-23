84922e62bf3916e204a11266dc2147dfa7791459
Augustine Osezua <augustineosezua1@gmail.com>
2025-08-23 19:26:23 -0400

    added new favicon, new text in typewriter

--- src/Typewrite.jsx
+++ src/Typewrite.jsx
@@ -1,22 +1,26 @@
 import Typewriter from "typewriter-effect";
 
 function TypeW() {
+  const strings = [
+    "Full Stack Developer",
+    "Computing Student at Queen's University",
+    "Curious Learner",
+    "Problem Solver",
+    "Creative Coder",
+    "Tech Enthusiast",
+    "Innovative Builder",
+    "Team Player",
+    "UI/UX Explorer",
+    "Resilient Developer",
+    "Driven Creator",
+    "Spiderman Fan",
+  ];
+
+  const randomStrings = strings.sort(() => 0.5 - Math.random()).slice(0, 6);
   return (
     <Typewriter
       options={{
-        strings: [
-          "Full Stack Developer",
-          "Computing Student at Queen's University",
-          "Curious Learner",
-          "Problem Solver",
-          "Creative Coder",
-          "Tech Enthusiast",
-          "Innovative Builder",
-          "Team Player",
-          "UI/UX Explorer",
-          "Resilient Developer",
-          "Driven Creator",
-        ],
+        strings: randomStrings,
         autoStart: true,
         loop: true,
       }}
